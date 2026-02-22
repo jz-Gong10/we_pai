815fa733ec1c60c343c325ba05394707d7454281
jz-Gong10 <16240058@qq.com>
2026-02-22 20:06:20 +0800

    接收自身信息

--- lib/ui/page/wode.dart
+++ lib/ui/page/wode.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:we_pai/service/api_service.dart';
 import 'package:we_pai/ui/widget/background.dart';
 import 'package:we_pai/ui/widget/up_edge.dart';
 import 'package:we_pai/ui/widget/user_show.dart';
 import 'package:we_pai/module/recieve_zishenxinxi.dart';
-import 'package:we_pai/shared_preference/user_storage.dart';
 
 class Wode extends StatefulWidget {
   const Wode({super.key});
@@ -13,50 +13,40 @@ class Wode extends StatefulWidget {
 }
 
 class _WodeState extends State<Wode> {
-  UserInfo? _user;
+  final ApiService _apiService = ApiService();
+  String? _error;
+  String name = '未知用户';
+  String casId = '000000';
+  String avatarUrl = '';
 
   @override
   void initState() {
     super.initState();
-    _loadUserData();
+    _loadData();
   }
 
-  Future<void> _loadUserData() async {
-    final user = await UserStorage.getUser();
+  Future<void> _loadData() async {
     setState(() {
-      _user = user;
+      _error = null;
     });
+    try {
+      // 并发请求示例
+      var result = await Future.wait([_apiService.getUserInfo()]);
+
+      setState(() {
+        name = result[0].name;
+        casId = result[0].casId;
+        avatarUrl = result[0].avatarUrl;
+      });
+    } catch (e) {
+      setState(() {
+        _error = e.toString();
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    String name = '未知用户';
-    String casId = '';
-    String? avatarUrl;
-
-    if (_user != null) {
-      setState(() {
-        name = _user!.name;
-        casId = _user!.casId;
-        avatarUrl = _user!.avatarUrl ?? '';
-      });
-    } else {
-      return Scaffold(
-        body: Stack(
-          children: [
-            Background(imagePath: 'lib/material/background2.png'),
-
-            Positioned(top: 72, left: 23, child: UpEdge(title: '个人主页')),
-
-            Positioned(
-              top: 155,
-              left: 25,
-              child: UserShow(name: '未知用户', casId: '000000', avatarUrl: ''),
-            ),
-          ],
-        ),
-      );
-    }
     return Scaffold(
       body: Stack(
         children: [
